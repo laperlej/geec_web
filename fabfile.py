5a052eea7500121dcf3137ddf2abe51bfb2bd2f0
Jonathan Laperle <jonathanlaperle@icloud.com>
2015-10-25 01:27:46 -0400

    added fabric script for local deployment

--- fabfile.py
+++ fabfile.py
@@ -2,4 +2,6 @@ from fabric.api import local
 
 def test():
     local('python manage.py test table --settings=geec_web.settings.development')
-    
+
+def local_deploy():
+    local('python manage.py runserver --settings=geec_web.settings.development')
