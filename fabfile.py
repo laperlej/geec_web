45e95bd64dae18fc1efc764e9300ec612a199a24
Jonathan Laperle <jonathanlaperle@icloud.com>
2015-10-25 05:35:34 -0400

    fix for tests

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local
 
 def test_local():
-    local('python manage.py test table --settings=geec_web.settings.development')
+    local('python manage.py test --settings=geec_web.settings.development')
 
 def deploy_local():
     local('python manage.py runserver --settings=geec_web.settings.development')
