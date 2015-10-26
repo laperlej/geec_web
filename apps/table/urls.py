e4bfb5772f90898c0a95c4ce62657c01cd6d07e4
Jonathan Laperle <jonathanlaperle@icloud.com>
2015-10-26 04:23:04 -0400

    file now sent to galaxy

--- apps/table/urls.py
+++ apps/table/urls.py
@@ -6,5 +6,5 @@ from views import MainView, DataView
 
 urlpatterns = [
     url(r'^$', MainView.as_view(), name='main_table'),
-    url(r'^data$', DataView.as_view(), name='download_page'),
+    url(r'^data/$', DataView.as_view(), name='download_page'),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
