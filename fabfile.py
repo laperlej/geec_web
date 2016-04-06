91b460b2fa41bb6eb31217db0e9e3820b5329d65
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-04-06 12:31:23 -0400

    added deploy_prod funciton to fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,9 @@ from fabric.api import local
 def test_local():
     local('python manage.py test --settings=geec_web.settings.development')
 
+def deploy_prod():
+    local('python manage.py collectstatic --noinput')
+
 def deploy_local():
     local('python manage.py runserver --settings=geec_web.settings.development')
 
