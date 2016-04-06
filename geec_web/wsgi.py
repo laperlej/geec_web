08c8565eacc41e50b8270f0e506519cc49672850
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-04-06 08:48:39 -0400

    test heroku

--- geec_web/wsgi.py
+++ geec_web/wsgi.py
@@ -5,7 +5,7 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/home/galaxy/geec_web/geec_web')
 sys.path.append("/home/galaxy/geec_web/Library/Python2.7/bin")
 sys.path.append("/home/galaxy/geec_web/.local/bin")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geec_web.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geec_web.settings.development")
 
 from dj_static import Cling
 application = Cling(get_wsgi_application())
