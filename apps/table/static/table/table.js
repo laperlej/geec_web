5ec96aecc09442a255bf50a32e708c1cceb20847
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-10-09 13:12:17 -0400

    bugfix

--- apps/table/static/table/table.js
+++ apps/table/static/table/table.js
@@ -362,9 +362,9 @@ $(document).ready(function() {
   $('#submit').on( "click", function () {
     var data = main_table.rows(':has(:checkbox:checked)').data();
     var text = release + "\n";
-    for (i = 0; i < data.length; i++) {
-      text += data[i].id + "\t";
-    }
+    //for (i = 0; i < data.length; i++) {
+      //text += data[i].id + "\t";
+    //}
     $("#datasets").val(text);
     $("#galaxy-form").submit();
   });
