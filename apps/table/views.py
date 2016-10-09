039530b463db3bb582db2d46bf5d6b4ff1deee65
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-10-09 13:41:35 -0400

    bugfix

--- apps/table/views.py
+++ apps/table/views.py
@@ -100,8 +100,8 @@ class DataView(View):
         json_content = selector_cache.get(data[0]).content
         datasets = data[1:]
         content = slice_json(json_content, datasets)
-        content = json_content
-        response = HttpResponse(json.dumps(content))
+        response = HttpResponse(datasets)
+        #response = HttpResponse(json.dumps(content))
         #data = request.POST.get('datasets', '')
         #response = HttpResponse(data)
         response['Content-Disposition'] = 'attachment; filename="dummy.txt"'
