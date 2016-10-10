bf0a29f0af515e146867bc04128baecc1cd2aa62
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-10-10 02:01:19 -0400

    bugfix

--- apps/table/static/table/table.js
+++ apps/table/static/table/table.js
@@ -366,6 +366,7 @@ $(document).ready(function() {
       text += data[i].id + "\n";
     }
     $("#datasets").val(text);
+    $("#out_title").val($('#out_file_name').val());
     $("#galaxy-form").submit();
   });
 
